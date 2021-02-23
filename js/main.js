905988cd5e91f8053506b234fa3587336212fe01
ゆーじろー <67575620+yujiroN@users.noreply.github.com>
2021-02-23 10:26:07 +0900

    first
    first

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// scroll
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var speed = 1000;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+});
